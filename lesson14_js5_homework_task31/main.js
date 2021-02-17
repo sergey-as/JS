408968c4641479ae55e639411632b8afa5746c86
sergey-as <sergey_as@ukr.net>
2021-02-17 20:33:36 +0200

    lesson14_js5_homework tasks 1-30  + lesson14_js5_homework_task31

--- lesson14_js5_homework_task31/main.js
+++ lesson14_js5_homework_task31/main.js
@@ -12,79 +12,27 @@ taskN.innerText = `----${task}----`;
 document.body.appendChild(taskN);
 
 console.log(document);
-{
-    let pById = document.getElementById('tsk24');
-    console.log(pById);
-    console.log(pById.innerText);
-    let pListByClassName = document.getElementsByClassName('task24');
-    console.log(pListByClassName);
-    for (let pListByClassNameElement of pListByClassName) {
-        console.log(pListByClassNameElement);
-        console.log(pListByClassNameElement.innerText);
+
+// // - знайти всі елементі, які мають class
+// let elements = document.querySelectorAll('[class]');
+// for (let elem of elements) {
+//     console.log(elem);
+// }
+
+// - знайти всі параграфи ,та змінити текст на hello oktenweb!
+let pPointsByTagName = document.getElementsByTagName('p');
+console.log(pPointsByTagName);
+for (let pPointsByTagNameElement of pPointsByTagName) {
+    console.log(pPointsByTagNameElement.innerText);
+    pPointsByTagNameElement.innerText = 'hello oktenweb!';
+    console.log(pPointsByTagNameElement.innerText);
     }
-    let pPointsByTagName = document.getElementsByTagName('h1');
-    console.log(pPointsByTagName);
-    for (let pPointsByTagNameElement of pPointsByTagName) {
-        console.log(pPointsByTagNameElement);
-        console.log(pPointsByTagNameElement.innerText);
-    }
+
+// - знайти всі div та змінити ім колір на червоний
+let divPointsByTagName = document.getElementsByTagName('div');
+// console.log(divPointsByTagName);
+for (let divPointsByTagNameElement of divPointsByTagName) {
+    divPointsByTagNameElement.style.background = '#ff9c47';
+    // divPointsByTagNameElement.style.background = 'red';
+    // console.log(divPointsByTagNameElement);
 }
-
-
-
-// ============
-// ====class===
-//     ============
-//
-//     - є сторінка rules.html. Контентом сторінки є заголовки та параграфи. Заголовки (h2) характеризують тему контенту яка вказана в параграфі.
-//     створити скрипт, котрий зчитує всі заголовки, та робить в блоці з id=content з них список(ul>li), який буде змістом того, що знаходиться на сторінці.
-//     Скріпт повинен працювати навіть якщо кількість блоків з заголовком та параграфом зміниться.
-//
-// -Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить з кожне правило в окремому блоці.
-//     При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
-//     Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
-//
-// let rules = [
-//     {
-//         title: 'Первое правило Бойцовского клуба.',
-//         body: 'Никому не рассказывать о Бойцовском клубе.'
-//     },
-//     {
-//         title: 'Второе правило Бойцовского клуба.',
-//         body: 'Никогда никому не рассказывать о Бойцовском клубе.'
-//     },
-//     {
-//         title: 'Третье правило Бойцовского клуба.',
-//         body: 'В схватке участвуют только двое.'
-//     },
-//     {
-//         title: 'Четвертое правило Бойцовского клуба.',
-//         body: 'Не более одного поединка за один раз.'
-//     },
-//     {
-//         title: 'Пятое правило Бойцовского клуба.',
-//         body: 'Бойцы сражаются без обуви и голые по пояс.'
-//     },
-//     {
-//         title: 'Шестое правило Бойцовского клуба.',
-//         body: 'Поединок продолжается столько, сколько потребуется.'
-//     },
-//     {
-//         title: 'Седьмое правило Бойцовского клуба.',
-//         body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
-//     },
-//     {
-//         title: 'Восьмое и последнее правило Бойцовского клуба.',
-//         body: 'Новичок обязан принять бой.'
-//     },
-//
-// ];
-//
-//
-//
-//
-// *** за допомогою fetch (як в прикладі) отримати від jsonplaceholder всі users. За допомогою document.createElement вивести їх в браузер. Помістити кожен окремий об'єкт в блок, при цьому кожен внутрішній об'єкт в свій блок (блок в блоці).
-// *** за допомогою fetch (як в прикладі) отримати від jsonplaceholder всі posts. За допомогою document.createElement вивести їх в браузер. Помістити кожен окремий об'єкт в блок, при цьому кожен внутрішній об'єкт(якщо він існує) в свій блок (блок в блоці).
-// *** за допомогою fetch (як в прикладі) отримати від jsonplaceholder всі comments. За допомогою document.createElement вивести їх в браузер. Помістити кожен окремий об'єкт в блок, при цьому кожен внутрішній об'єкт(якщо він існує) в свій блок (блок в блоці).
-// ****** при помощи fetch (как в примере) получить от jsonplaceholder все posts. Внутри последнего then() сделать еще один fetch который сделает запрос и получит все comments. Объеденить соответсвующий post с соответсвующими comment и вывести в браузер. Подсказка : в каждом comment есть поле postId которое определяет какой комментарий принадлежит какому посту
-// © 2021 GitHub, Inc.
