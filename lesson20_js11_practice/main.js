5544550d5112eb642348d61de83a030561bd77ed
sergey-as <sergey_as@ukr.net>
2021-03-05 21:12:54 +0200

    lesson20_js11_practice complete

--- lesson20_js11_practice/main.js
+++ lesson20_js11_practice/main.js
@@ -207,3 +207,226 @@ let div4 = task(4, '' +
     ' При нажати на нее появляется форма, в которой есть все необходимые инпуты для редактирования,' +
     ' которые уже заполнены данными объекта' +
     '');
+{
+    let phoneBook = [];
+    let usersForInitiate = [
+        {
+            name: "Leanne Graham",
+            phone: "1-770-736-8031 x56442",
+            email: "Sincere@april.biz",
+            companyName: "Romaguera-Crona",
+            companyDepartment: "Multi-layered client-server neural-net",
+            dateOfBirth: "01.02.1991"
+        },
+        {
+            name: "Ervin Howell",
+            phone: "010-692-6593 x09125",
+            email: "Shanna@melissa.tv",
+            companyName: "Deckow-Crist",
+            companyDepartment: "Proactive didactic contingency",
+            dateOfBirth: "18.05.1997"
+        },
+        {
+            name: "Clementine Bauch",
+            phone: "1-463-123-4447",
+            email: "Nathan@yesenia.net",
+            companyName: "Romaguera-Jacobson",
+            companyDepartment: "Face to face bifurcated interface",
+            dateOfBirth: "28.11.1990"
+        },
+        {
+            name: "Patricia Lebsack",
+            phone: "493-170-9623 x156",
+            email: "Julianne.OConner@kory.org",
+            companyName: "Robel-Corkery",
+            companyDepartment: "Multi-tiered zero tolerance productivity",
+            dateOfBirth: "08.10.1989"
+        },
+        {
+            name: "Chelsey Dietrich",
+            phone: "(254)954-1289",
+            email: "Lucio_Hettinger@annie.ca",
+            companyName: "Keebler LLC",
+            companyDepartment: "User-centric fault-tolerant solution",
+            dateOfBirth: "05.04.1980"
+        },
+        {
+            name: "Mrs. Dennis Schulist",
+            phone: "1-477-935-8478 x6430",
+            email: "Karley_Dach@jasper.info",
+            companyName: "Considine-Lockman",
+            companyDepartment: "Synchronised bottom-line interface",
+            dateOfBirth: "05.04.1980"
+        },
+        {
+            name: "Kurtis Weissnat",
+            phone: "210.067.6132",
+            email: "Telly.Hoeger@billy.biz",
+            companyName: "Johns Group",
+            companyDepartment: "Configurable multimedia task-force",
+            dateOfBirth: "03.07.1981"
+        },
+        {
+            name: "Nicholas Runolfsdottir V",
+            phone: "586.493.6943 x140",
+            email: "Sherwood@rosamond.me",
+            companyName: "Abernathy Group",
+            companyDepartment: "Implemented secondary concept",
+            dateOfBirth: "03.09.1983"
+        },
+        {
+            name: "Glenna Reichert",
+            phone: "(775)976-6794 x41206",
+            email: "Chaim_McDermott@dana.io",
+            companyName: "Yost and Sons",
+            companyDepartment: "Switchable contextually-based project",
+            dateOfBirth: "12.29.1973"
+        },
+        {
+            name: "Clementina DuBuque",
+            phone: "024-648-3804",
+            email: "Rey.Padberg@karina.biz",
+            companyName: "Hoeger LLC",
+            companyDepartment: "Centralized empowering task-force",
+            dateOfBirth: "11.09.1977"
+        },
+    ];
+
+    function initiate(users) {
+        phoneBook.splice(0, phoneBook.length);
+        for (let i = 0; i < users.length; i++) {
+            let user = users[i];
+            phoneBook.push(`${user.name} ${user.phone}`);
+            localStorage.setItem('phoneBook' + i, JSON.stringify(user));
+        }
+        showPhoneBook();
+    };
+
+    function removeItem() {
+        for (let i = 0; i < phoneBook.length; i++) {
+            localStorage.removeItem('phoneBook' + i);
+        }
+        initiate([]);
+    };
+
+    let p0 = createElem('p', div4, '', '', '', '', '', '');
+    let btnInitiate = createElem('input', p0, 'btnInitiate', '', '', 'button', '', 'Initiate');
+    let btnRemoveItem = createElem('input', p0, 'btnRemoveItem', '', '', 'button', '', 'Remove Items');
+    btnInitiate.onclick = () => initiate(usersForInitiate);
+    btnRemoveItem.onclick = () => removeItem();
+
+
+    let ul = createElem('ul', div4, 'phoneBook', 'phoneBook', '', '', '', '');
+
+    function fillArrObjPhoneBook() {
+        let objPhoneBook = [];
+        let keys = Object.keys(localStorage);
+        for (let key of keys) {
+            let partOfKey = key.slice(0, 'phoneBook'.length);
+            if (partOfKey === 'phoneBook') {
+                let id = +key.slice('phoneBook'.length);
+                objPhoneBook[id] = JSON.parse(localStorage.getItem(key));
+            }
+        }
+        return objPhoneBook;
+    }
+
+    function clearUl(id) {
+        let ulPhoneBook = document.getElementById(id);
+        while (ulPhoneBook.firstChild) {
+            ulPhoneBook.removeChild(ulPhoneBook.firstChild);
+        }
+        return ulPhoneBook;
+    }
+
+    function showPhoneBook() {
+        let objPhoneBook = fillArrObjPhoneBook();
+        let ulPhoneBook = clearUl('phoneBook');
+
+        for (let i = 0; i < objPhoneBook.length; i++) {
+            let user = objPhoneBook[i];
+            phoneBook[i] = `${user.name} ${user.phone}`;
+        }
+
+
+        for (let i = 0; i < phoneBook.length; i++) {
+            let phoneBookElement = phoneBook[i];
+            let p = createElem('p', ulPhoneBook, '', '', '', '', '', '');
+            let li = createElem('li', p, '', phoneBookElement, '', '', '', '');
+            let btnDel = createElem('input', p, '', '', '', 'button', '', 'Delete');
+            btnDel.onclick = () => deleteElem(i);
+            let btnEdit = createElem('input', p, '', '', '', 'button', '', 'Edit');
+            btnEdit.onclick = () => editElem(i);
+        }
+    }
+
+    showPhoneBook();
+
+    function deleteElem(ind) {
+        let objPhoneBook = fillArrObjPhoneBook();
+        objPhoneBook.splice(ind, 1);
+        removeItem();
+        initiate(objPhoneBook);
+        showPhoneBook();
+    }
+
+    function editElem(ind) {
+        let objPhoneBook = fillArrObjPhoneBook();
+
+        let edEl = objPhoneBook[ind];
+        for (let edElKey in edEl) {
+            let el = document.getElementById(edElKey);
+            el.value = edEl[edElKey];
+        }
+        localStorage.setItem('currentEditingElement', ind);
+    }
+
+    function saveElem() {
+        let objPhoneBook = fillArrObjPhoneBook();
+        let ind = localStorage.getItem('currentEditingElement');
+        (ind === null) && (ind = objPhoneBook.length);
+        // console.log(ind);
+
+        let saveEl = {};
+        saveEl.name = document.getElementById('name').value;
+        saveEl.phone = document.getElementById('phone').value;
+        saveEl.email = document.getElementById('email').value;
+        saveEl.companyName = document.getElementById('companyName').value;
+        saveEl.companyDepartment = document.getElementById('companyDepartment').value;
+        saveEl.dateOfBirth = document.getElementById('dateOfBirth').value;
+        objPhoneBook[+ind] = saveEl;
+        removeItem();
+        initiate(objPhoneBook);
+        showPhoneBook();
+    }
+
+    function newElem() {
+        localStorage.removeItem('currentEditingElement');
+        document.getElementById('name').value = '';
+        document.getElementById('phone').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('companyName').value = '';
+        document.getElementById('companyDepartment').value = '';
+        document.getElementById('dateOfBirth').value = '';
+    }
+
+
+    let form = createElem('form', div4, 'myForm4', '', '', '', 'form', '');
+    let p1 = createElem('p', form, '', 'name:', '', '', '', '');
+    let name = createElem('input', p1, 'name', '', '', 'text', 'name', '');
+    let p2 = createElem('p', form, '', 'phone:', '', '', '', '');
+    let phone = createElem('input', p2, 'phone', '', '', 'text', 'phone', '');
+    let p3 = createElem('p', form, '', 'email:', '', '', '', '');
+    let email = createElem('input', p3, 'email', '', '', 'text', 'email', '');
+    let p4 = createElem('p', form, '', 'companyName:', '', '', '', '');
+    let companyName = createElem('input', p4, 'companyName', '', '', 'text', 'companyName', '');
+    let p5 = createElem('p', form, '', 'companyDepartment:', '', '', '', '');
+    let companyDepartment = createElem('input', p5, 'companyDepartment', '', '', 'text', 'companyDepartment', '');
+    let p6 = createElem('p', form, '', 'dateOfBirth:', '', '', '', '');
+    let dateOfBirth = createElem('input', p6, 'dateOfBirth', '', '', 'text', 'dateOfBirth', '');
+
+    let btnSave = createElem('input', div4, 'btnSave', '', '', 'button', '', 'Save');
+    btnSave.onclick = () => saveElem();
+    let btnNew = createElem('input', div4, 'btnNew', '', '', 'button', '', 'New');
+    btnNew.onclick = () => newElem();
+}
