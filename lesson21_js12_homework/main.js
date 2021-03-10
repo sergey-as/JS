d9b9c3bdb2161b6711b7602aebe673b115b65af9
sergey-as <sergey_as@ukr.net>
2021-03-10 20:42:46 +0200

    lesson21_js12_homework complete

--- lesson21_js12_homework/main.js
+++ lesson21_js12_homework/main.js
@@ -23,7 +23,6 @@ let div1 = task(1, '- Реалізувати друкарську машинку
     '\n о (затримка 1)' +
     '');
 {
-
     let p = document.createElement('p');
     div1.appendChild(p);
     let input = document.createElement('input');
@@ -34,7 +33,7 @@ let div1 = task(1, '- Реалізувати друкарську машинку
         min = Math.ceil(min);
         max = Math.floor(max);
         let timeout = (Math.floor(Math.random() * (max - min + 1)) + min) * 100; //Максимум и минимум включаются
-        console.log(timeout);
+        // console.log(timeout);
         return timeout;
     }
 
@@ -44,7 +43,7 @@ let div1 = task(1, '- Реалізувати друкарську машинку
         }, getTimeout());
     })
         .then(value => {
-            console.log(value);
+            // console.log(value);
             input.value += value;
             return new Promise(resolve => {
                 setTimeout(() => {
@@ -53,7 +52,7 @@ let div1 = task(1, '- Реалізувати друкарську машинку
             });
         })
         .then(value => {
-            console.log(value);
+            // console.log(value);
             input.value += value;
             return new Promise(resolve => {
                 setTimeout(() => {
@@ -62,7 +61,7 @@ let div1 = task(1, '- Реалізувати друкарську машинку
             });
         })
         .then(value => {
-            console.log(value);
+            // console.log(value);
             input.value += value;
             return new Promise(resolve => {
                 setTimeout(() => {
@@ -71,7 +70,7 @@ let div1 = task(1, '- Реалізувати друкарську машинку
             });
         })
         .then(value => {
-            console.log(value);
+            // console.log(value);
             input.value += value;
             return new Promise(resolve => {
                 setTimeout(() => {
@@ -80,9 +79,37 @@ let div1 = task(1, '- Реалізувати друкарську машинку
             });
         })
         .then(value => {
-            console.log(value)
+            // console.log(value)
             input.value += value;
         });
+}
+
+
+//2-5
+function createElem(type, id = '', innerText = '', parent, background = '') {
+    let elem = document.createElement(type);
+    !(id === '') && (elem.id = id);
+    !(innerText === '') && (elem.innerText = innerText);
+    !(background === '') && (elem.style.background = background);
+    parent.appendChild(elem);
+    return elem;
+}
+
+function processObject(obj, parent) {
+    for (let objKey in obj) {
+        let objElement = obj[objKey];
+        if (typeof objElement === 'object') {
+            background = '';
+            (parent.style.background === '#8abdd5') && (background = 'red');
+            (parent.style.background === 'silver') && (background = '#8abdd5');
+            (parent.style.background === '') && (background = 'silver');
+
+            let objBlock = createElem('div', '', objKey + ':', parent, background);
+            processObject(objElement, objBlock);
+        } else {
+            createElem('p', '', objKey + ': ' + objElement, parent);
+        }
+    }
 }
 
 
@@ -92,19 +119,49 @@ let div2 = task(2, '-за допомогою fetch (як в прикладі) о
     '\n Помістити кожен окремий об\'єкт в блок, при цьому кожен внутрішній об\'єкт в свій блок (блок в блоці).' +
     '');
 
+{
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            for (let jsonElement of json) {
+                processObject(jsonElement, createElem('div', '', '', div2));
+            }
+        });
+}
+
 
 //3
 let div3 = task(3, '- за допомогою fetch (як в прикладі) отримати від jsonplaceholder всі posts.' +
     '\n За допомогою document.createElement вивести їх в браузер.' +
     '\n Помістити кожен окремий об\'єкт в блок, при цьому кожен внутрішній об\'єкт(якщо він існує) в свій блок (блок в блоці).' +
     '');
-
+{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            for (let jsonElement of json) {
+                processObject(jsonElement, createElem('div', '', '', div3));
+            }
+        });
+}
 
 //4
 let div4 = task(4, '- за допомогою fetch (як в прикладі) отримати від jsonplaceholder всі comments.' +
     '\n За допомогою document.createElement вивести їх в браузер.' +
     '\n Помістити кожен окремий об\'єкт в блок, при цьому кожен внутрішній об\'єкт(якщо він існує) в свій блок (блок в блоці).' +
     '');
+{
+    fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            for (let jsonElement of json) {
+                processObject(jsonElement, createElem('div', '', '', div4));
+            }
+        });
+}
 
 
 //5
@@ -114,3 +171,24 @@ let div5 = task(5, '-при помощи fetch (как в примере) пол
     '\n Подсказка : в каждом comment есть поле postId которое определяет какой комментарий принадлежит какому посту' +
     '\n часть из них уже кто-то делал , так вот, пофиг , что авы их уже делали, делаем еще раз, не копипастим!!!!!!!!!!! >:-()' +
     '');
+{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(posts => {
+            // console.log('posts', posts);
+            return posts;
+        })
+        .then(function (posts) {
+                for (let post of posts) {
+                    fetch('https://jsonplaceholder.typicode.com/comments?postId=' + post.id)
+                        .then(response => response.json())
+                        .then(comments => {
+                            post.comments = comments;
+                            console.log('post', post);
+                            processObject(post, createElem('div', '', '', div5));
+                        });
+                    // break;
+                }
+            }
+        )
+}
